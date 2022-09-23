dc9aa6471f4ffeeb50ad400d4766f74e718d5463
Sakura 森川 <morikawa@kimisui56.work>
2022-09-23 10:18:12 +0800

    添加项目文件。

---
+++ proj1_2/源.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "This is my first program!";
+	cout << "\n";
+	cout << "Study hard!\n";
+
+	return 0;
+}
