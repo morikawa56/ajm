979c7219c0b30da01374f1543188604722f58680
Sakura 森川 <morikawa@kimisui56.work>
2022-09-23 10:26:49 +0800

    添加项目文件。

---
+++ proj1_3/ex1_3.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+const double PI = 3.1415927;
+int main()
+{
+	float area, r;
+	r = 4.0;
+	area = 2 * PI * r * r;
+	cout << "\n the area of yuan is" << area << endl;
+
+	return 0;
+}
