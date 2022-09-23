58824766b11326f67c5efa8b138660297f9af84e
Sakura 森川 <morikawa@kimisui56.work>
2022-09-23 10:33:01 +0800

    添加项目文件。

---
+++ proj1_4/ex1_4.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\n";
+	cout << "    *\n";
+	cout << "   * *\n";
+	cout << "  *   *\n";
+	cout << " *     *\n";
+	cout << "*********\n";
+
+	return 0;
+}
